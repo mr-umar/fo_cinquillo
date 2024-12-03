5c0ae503b668585de9471f9d4ea84c892bcce73a
Umar Mohammad <mrumar628@gmail.com>
2024-12-03 14:15:17 +0100

    Fix formatting in imprimir_mantel and add comment to eliminar_carta_jugador for clarity

--- mesa.c
+++ mesa.c
@@ -6,7 +6,7 @@ void imprimir_mantel(int mantel[NUM_NUMS][NUM_PALS]) {
     for (int i = 0; i < NUM_NUMS; i++) {
         for (int j = 0; j < NUM_PALS; j++) {
             if (mantel[i][j]) {
-                printf("[ %2d ]", i + 1);
+                printf("[ %.2d ]", i + 1);
             } else {
                 printf("[- -] ");
             }
--- partida.c
+++ partida.c
@@ -76,7 +76,7 @@ int poner_carta(t_carta carta, int num_jugador, t_jugador jugadores[NUM_JUGS], i
     return jugadores[num_jugador].num_cartas == 0;
 }
 
-void eliminar_carta_jugador(t_carta carta, int num_jugador, t_jugador jugadores[NUM_JUGS]) {
+void eliminar_carta_jugador(t_carta carta, int num_jugador, t_jugador jugadores[NUM_JUGS]) { // elimina la carta de la baraja del jugador despues de la jugada
     int index = -1;
     for (int i = 0; i < jugadores[num_jugador].num_cartas; i++) {
         if (jugadores[num_jugador].cartas[i].pal == carta.pal && jugadores[num_jugador].cartas[i].num == carta.num) {
