2af451919069c0aebf466036a75eb57f50d0ad27
Umar Mohammad <mrumar628@gmail.com>
2024-12-04 16:10:20 +0100

    Add color formatting to mantel display in imprimir_mantel function

--- mesa.c
+++ mesa.c
@@ -1,12 +1,34 @@
 #include "mesa.h"
+#include "colores.h"
 #include <stdio.h>
 
 void imprimir_mantel(int mantel[NUM_NUMS][NUM_PALS]) {
+    // Encabezado de los palos
     printf("\n[ORO] [COP] [ESP] [BAS]\n");
-    for (int i = 0; i < NUM_NUMS; i++) {
-        for (int j = 0; j < NUM_PALS; j++) {
-            if (mantel[i][j]) {
-                printf("[ %2d ]", i + 1);
+
+    for (int num = 0; num < NUM_NUMS; num++) {
+        for (int pal = 0; pal < NUM_PALS; pal++) {
+            if (mantel[num][pal]) {
+                // Aplicar el color según el palo usando funciones disponibles en colores.h
+                switch (pal) {
+                    case 0: 
+                        printf_color_num(3); // Oros en amarillo (código ANSI 3)
+                        printf("[O%2d] ", num + 1); 
+                        break;
+                    case 1: 
+                        printf_color_num(1); // Copas en rojo (código ANSI 1)
+                        printf("[C%2d] ", num + 1); 
+                        break;
+                    case 2: 
+                        printf_color_num(6); // Espadas en cyan (código ANSI 6)
+                        printf("[E%2d] ", num + 1); 
+                        break;
+                    case 3: 
+                        printf_color_num(2); // Bastos en verde (código ANSI 2)
+                        printf("[B%2d] ", num + 1); 
+                        break;
+                }
+                printf_reset_color(); // Reiniciar el color
             } else {
                 printf("[- -] ");
             }
